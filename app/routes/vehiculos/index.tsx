49c21ae5c64ec570191806bf115f9af3587933b5
JuceGT <julio2201@gmail.com>
2023-02-16 17:21:54 -0600

    fix: fixed vehicles route error

--- app/routes/vehiculos/index.tsx
+++ app/routes/vehiculos/index.tsx
@@ -1,12 +1,19 @@
 import { Link } from '@remix-run/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Plate from '~/components/plate';
-import useLocalStorage from '~/hooks/use-local-storage';
 import { IVehicle } from '~/interfaces/IVehicle';
 import DefaultLayout from '~/layouts/default';
 
 export default function Vehicles() {
-  const [vehicles] = useLocalStorage<IVehicle[]>('vehicles', []);
+  const [vehicles, setVehicles] = useState<IVehicle[]>([]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const localVehicles = localStorage.getItem('vehicles');
+
+      if (localVehicles) setVehicles(JSON.parse(localVehicles));
+    }
+  }, []);
   return (
     <DefaultLayout>
       <p>Pagina de Vehiculos</p>
