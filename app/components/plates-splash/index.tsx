773cdd93361f953f6af1b2c21faebc48bb1811de
JuceGT <julio2201@gmail.com>
2023-02-22 14:02:29 -0600

    fix: plate notification

--- app/components/plates-splash/index.tsx
+++ app/components/plates-splash/index.tsx
@@ -4,9 +4,9 @@ import { PlatesSplashWrapper } from './style';
 const PlatesSplash = () => {
   return (
     <PlatesSplashWrapper>
-      <Plate notForm type="C" number="960PAS" notification={2} />
-      <Plate notForm type="M" number="654GHJ" notification={1} />
-      <Plate notForm type="P" number="832RYM" notification={3} />
+      <Plate notForm type="C" number="960PAS" noti={2} />
+      <Plate notForm type="M" number="654GHJ" noti={1} />
+      <Plate notForm type="P" number="832RYM" noti={3} />
     </PlatesSplashWrapper>
   );
 };
