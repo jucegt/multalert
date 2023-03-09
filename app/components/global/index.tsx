64770d5cc82e4de472824a08f0fbbd8591746cc9
JuceGT <julio2201@gmail.com>
2023-03-09 17:30:41 -0600

    feat: changed global styles

--- app/components/global/index.tsx
+++ app/components/global/index.tsx
@@ -1,16 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+		min-width: 320px;
+		font-size: 16px;
+    font-family: 'Barlow';
+	}
   *, *:before, *:after {
     position: relative;
-    box-sizing: border-box;
+		box-sizing: border-box;
   }
   body {
-    background: #ccc;
     margin: 0;
-    padding: 0;
-    font-family: 'Barlow';
+		padding: 0;
+		-webkit-tap-highlight-color: transparent;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
   }
+  input, select, textarea, button {
+    font-family: 'Barlow';
+		appearance: none;
+	}
+  select::-ms-expand { display: none; }
 `;
 
 export default GlobalStyles;
