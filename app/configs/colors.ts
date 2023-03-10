e9e61580a60edbb31d7ea83d5e794d38bdd9f3ef
jucegt <julio2201@gmail.com>
2023-03-09 22:24:00 -0600

    feat: added config colors

---
+++ app/configs/colors.ts
@@ -0,0 +1,11 @@
+enum colors {
+  white = '#fff',
+  black = '#000',
+  red = '#eb5757',
+  blue = '#2575fc',
+  purple = '#6a11cb',
+  darkBlue = '#0e0f13',
+  lightBlue = '#d0daff',
+}
+
+export default colors;
