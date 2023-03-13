159c4ab42a81382bcccc6a1cebbe7979a6a925d7
jucegt <julio2201@gmail.com>
2023-03-13 00:49:34 -0600

    feat: changed global styles

--- app/components/global/index.tsx
+++ app/components/global/index.tsx
@@ -1,33 +1,36 @@
 import { createGlobalStyle } from 'styled-components';
 import colors from '~/configs/colors';
+import toRem from '~/utils/to-rem';
 
 const GlobalStyles = createGlobalStyle`
   :root {
 		min-width: 320px;
 		font-size: 16px;
     font-family: 'Barlow';
-    --bg-color: ${colors.lightBlue};
-    --text-color: ${colors.gray};
-    --text-highlighted: ${colors.red};
-    --primary-gradient: ${colors.defaultGradient};
-    --bg-button: var(---primary-gradient);
-    --bg-button-secondary: ${colors.red};
-    --bg-button-cancel: ${colors.cancelGradientLight};
+    --bg-box-light: ${colors.lightBoxLight};
     --bg-box: ${colors.lightBox};
-    --decoration-color: ${colors.blue};
-    --notification-bg: ${colors.red};
-    --delete-color: ${colors.red};
+    --bg-button-cancel: ${colors.cancelGradientLight};
+    --bg-button-secondary: ${colors.red};
+    --bg-button: var(---primary-gradient);
+    --bg-color: ${colors.lightBlue};
     --bg-success: ${colors.green};
     --bg-warning: ${colors.yellow};
-    --bg-box-light: ${colors.lightBoxLight};
+    --decoration-color: ${colors.blue};
+    --delete-color: ${colors.red};
+    --notification-bg: ${colors.red};
+    --primary-gradient: ${colors.defaultGradient};
+    --text-color: ${colors.gray};
+    --text-highlighted: ${colors.red};
+    --text-shadow: none;
 	}
   .dark-mode {
     :root {
+      --bg-box-light: ${colors.darkBoxLight};
+      --bg-box: ${colors.darkBox};
+      --bg-button-cancel: ${colors.cancelGradientDark};
       --bg-color: ${colors.darkBlue};
       --text-color: ${colors.white};
-      --bg-button-cancel: ${colors.cancelGradientDark};
-      --bg-box: ${colors.darkBox};
-      --bg-box-light: ${colors.darkBoxLight};
+      --text-shadow: 0 ${toRem(2)} ${toRem(2)} rgba(0, 0, 0, 0.4);
     }
   }
   *, *:before, *:after {
