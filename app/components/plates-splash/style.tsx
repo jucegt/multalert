478c4ef09ba427eacbaae0eae390f4480c4d384e
jucegt <julio2201@gmail.com>
2023-03-13 00:57:40 -0600

    feat: changed styles on plates splash

--- app/components/plates-splash/style.tsx
+++ app/components/plates-splash/style.tsx
@@ -3,25 +3,26 @@ import toRem from '~/utils/to-rem';
 import { PlateWrapper } from '../plate/style';
 
 export const PlatesSplashWrapper = styled.div`
-  margin: ${toRem(30)} 0 ${toRem(-200)};
+  margin: ${toRem(30)} ${toRem(-20)} -60%;
   left: 50%;
-  transform: scale(0.7) translateX(-60%);
+  transform: scale(0.7) translateX(calc(-50% + 20px));
   transform-origin: top left;
   ${PlateWrapper} {
-    margin: 0;
+    margin: 0 auto;
     box-shadow: none;
-    transform: perspective(700px) rotateY(45deg) rotate(10deg) translateY(-20%);
+    transform: perspective(${toRem(700)}) rotateY(45deg) rotate(10deg)
+      translate(-3%, -20%);
     &:first-child {
-      transform: perspective(700px) rotateY(-45deg) rotate(-10deg) scale(0.6)
-        translate(50%, 25%);
+      transform: perspective(${toRem(700)}) rotateY(-45deg) rotate(-10deg)
+        scale(0.6) translate(35%, 25%);
     }
     &:last-child {
-      transform: perspective(700px) rotateY(-20deg) rotate(-5deg) scale(0.8)
-        translate(20%, -30%);
+      transform: perspective(${toRem(700)}) rotateY(-20deg) rotate(-5deg)
+        scale(0.8) translate(8%, -30%);
     }
   }
 
   @media screen and (min-width: ${toRem(500)}) {
-    margin: 0 ${toRem(-16)} ${toRem(-200)};
+    margin: ${toRem(30)} 0 -42%;
   }
 `;
