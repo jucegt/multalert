806a2bb59db0625901c183fddd4a0359483687e3
JuceGT <julio2201@gmail.com>
2023-03-17 15:49:56 -0600

    feat: added info

---
+++ app/components/svgs/info.tsx
@@ -0,0 +1,20 @@
+const IconInfo = () => {
+  return (
+    <svg
+      strokeWidth={1.5}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+    >
+      <path
+        d="M12 11.5v5m0-8.99.01-.011M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default IconInfo;
