563b71bfdad109e976b3603dfa8cf6501020e9e9
JuceGT <julio2201@gmail.com>
2023-03-17 15:50:06 -0600

    feat: added location

---
+++ app/components/svgs/location.tsx
@@ -0,0 +1,25 @@
+const IconLocation = () => {
+  return (
+    <svg
+      strokeWidth={1.5}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+    >
+      <path
+        d="M20 10c0 4.418-8 12-8 12s-8-7.582-8-12a8 8 0 1 1 16 0z"
+        stroke="currentColor"
+      />
+      <path
+        d="M12 11a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default IconLocation;
