4f30b53206963b0fa88918b6ed07134e94e9ad5c
JuceGT <julio2201@gmail.com>
2023-03-17 00:29:57 -0600

    feat: added funny name to vehicle name placeholder

--- app/components/vehicle-info/index.tsx
+++ app/components/vehicle-info/index.tsx
@@ -1,22 +1,22 @@
+import { names } from '~/data/vehicle-names.json';
+
 import Button from '~/components/button';
 import ButtonGroup from '~/components/button-group';
 import IconVehicle from '~/components/svgs/vehicle';
 import Input from '~/components/form-element/input';
 import Label from '~/components/form-element/label';
 import Plate from '~/components/plate';
+import randomInt from '~/utils/random-int';
 
 const VehicleInfo = () => {
+  const funnyName = names[randomInt(names.length - 1)];
+  const placeholder = `Ej.: ${funnyName}`;
   return (
     <>
       <Label htmlFor="vName" required>
         Nombre:
       </Label>
-      <Input
-        id="vName"
-        name="vName"
-        placeholder="Ej.: Rayo McQueen"
-        type="text"
-      />
+      <Input id="vName" name="vName" placeholder={placeholder} type="text" />
       <Label
         htmlFor="pNumber"
         required
