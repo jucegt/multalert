66290ec525c4bf49b73797d8863b04ea35029a57
JuceGT <julio2201@gmail.com>
2023-03-17 15:50:54 -0600

    feat: added meta description

--- app/configs/meta-data.ts
+++ app/configs/meta-data.ts
@@ -3,6 +3,8 @@ import { MetaFunction } from '@remix-run/node';
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title:
-    'multalert - Recibe notificaciones cuando te coloquen una multa de tránsito en Emetra',
+    'multalert - Recibe notificaciones cuando te coloquen una multa de tránsito en EMETRA',
   viewport: 'width=device-width,initial-scale=1',
+  description:
+    'Recibe notificaciones cuando te coloquen una multa de tránsito en EMETRA',
 });
