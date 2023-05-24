bb513873401b55de5a734ac445f750fcd64d9b1b
JuceGT <julio2201@gmail.com>
2023-05-24 14:21:05 -0600

    fix: fixed emetra service

--- app/services/emetra.ts
+++ app/services/emetra.ts
@@ -16,13 +16,11 @@ export const getEmetraInfo = async (type: string, number: string) => {
   });
   const htmlString = await emetraRequest.text();
 
-  console.log(htmlString);
-
   const plateString = `${type}-${number}`.toUpperCase();
 
   const html = parse(htmlString);
-  const tables = html.querySelectorAll('table');
-  const infoTable = tables[0].querySelector('tbody');
+  const tables = html?.querySelectorAll('table');
+  const infoTable = tables[0]?.querySelector('tbody');
   const info = infoTable?.innerText
     .replace(/(\r\n|\n|\r)/gm, ' ')
     .replace(/\s+/g, ' ')
@@ -32,7 +30,7 @@ export const getEmetraInfo = async (type: string, number: string) => {
     .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())
     .trim();
 
-  const finesTable = html.getElementById('transito');
+  const finesTable = html?.getElementById('transito');
   const fines = finesTable ? emetraToJson(finesTable) : [];
 
   return {
